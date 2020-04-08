36a45ed94d6ad9dd0ca20b436a58d6a346136728
Johann Delebarre <johann.delebarre@letsco.co>
2020-04-08 11:01:13 +0200

    default empty sources

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # campus-2021-JS-exo-complet
+
+Cliquer sur le bouton "Charger la liste" doit :
+1. Gérer le click sur le bouton
+2. Appeler une api
+3. Pour chaque élément de la liste, ajouter un élément au futur carousel
+4. Déclencer le carousel !
--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,13 @@
 
 function fillTheCarousel(jsondata)
 {
-    let carousel = document.querySelector('#carousel');
-    jsondata.forEach((element) => {
-        let carouselItem = document.createElement('div');
-        carouselItem.innerHTML = element.body;
-        carousel.appendChild(carouselItem);
-    });
 }
 
 $(document).ready(function()
 {
     // Cliquer sur le bouton "Charger la liste" doit :
     //  1. Gérer le click sur le bouton
-    let declencheur = document.querySelector('#declencheur');
-    declencheur.addEventListener('click', function(event) {
-        //  2. Appeler une api
-        fetch('https://jsonplaceholder.typicode.com/users/1/posts')
-
-        //  3. Pour chaque élément de la liste, ajouter un élément au futur carousel
-        .then(response => response.json())
-        .then(json => {
-            fillTheCarousel(json);
-
-            //  4. Déclencer le carousel !
-            $('#carousel').carouFredSel({
-                responsive: true,
-                align: "center",
-                direction: 'left'
-            });
-        });
-    });
+    //  2. Appeler une api
+    //  3. Pour chaque élément de la liste, ajouter un élément au futur carousel
+    //  4. Déclencer le carousel !
 });
