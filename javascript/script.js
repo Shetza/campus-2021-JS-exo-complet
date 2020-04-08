a5096be32d1fe838fb34b83a54cd67d34fe2e9bb
COCCHI Simon <59911613+simoncocchi@users.noreply.github.com>
2020-04-08 15:16:43 +0200

    fin du code avec aide de johann

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,34 @@
 
-function fillTheCarousel(jsondata)
-{
+function fillTheCarousel(jsondata) {
+    let carouselSelector = document.querySelector('#carousel');
+    jsondata.forEach(element => {
+        console.log('ca creer le code')
+        let test = carouselSelector.appendChild(creatediv());
+        test.appendChild(createtitre(element));
+    });
 }
 
-$(document).ready(function()
-{
-    // Cliquer sur le bouton "Charger la liste" doit :
-    //  1. Gérer le click sur le bouton
-    //  2. Appeler une api (https://jsonplaceholder.typicode.com/users/1/posts)
-    //  3. Pour chaque élément de la liste, ajouter un élément au futur carousel
-    
-    //  4. Déclencher le carousel
-    $('#carousel').carouFredSel({
-        responsive: true,
-        align: "center",
-        direction: 'left'
-    });
+$(document).ready(function () {
+    document.querySelector('#declencheur').addEventListener('click', function () { // ajout d'un event lors du clique sur le bouton 
+    fetch('https://jsonplaceholder.typicode.com/users/1/posts')
+        .then(response => response.json())
+        .then(json => { fillTheCarousel(json); });
+
+        $('#carousel').carouFredSel({
+            responsive: true,
+            align: "center",
+            direction: 'left'
+        });
+})
 });
+
+function creatediv() {
+    let simpledivCreate = document.createElement('div');
+    return simpledivCreate
+}
+
+function createtitre(data) {
+    let h1Create = document.createElement('h1');
+    h1Create.textContent = data['title']
+    return h1Create
+}
