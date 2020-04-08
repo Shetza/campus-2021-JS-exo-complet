dc5c665306cb2a32f4eee225674129c80cd7160c
Shetza <unknownshetza@gmail.com>
2020-04-08 11:32:24 +0200

    Add carousel launch code

--- javascript/script.js
+++ javascript/script.js
@@ -7,7 +7,13 @@ $(document).ready(function()
 {
     // Cliquer sur le bouton "Charger la liste" doit :
     //  1. Gérer le click sur le bouton
-    //  2. Appeler une api
+    //  2. Appeler une api (https://jsonplaceholder.typicode.com/users/1/posts)
     //  3. Pour chaque élément de la liste, ajouter un élément au futur carousel
-    //  4. Déclencer le carousel !
+    
+    //  4. Déclencher le carousel
+    $('#carousel').carouFredSel({
+        responsive: true,
+        align: "center",
+        direction: 'left'
+    });
 });
