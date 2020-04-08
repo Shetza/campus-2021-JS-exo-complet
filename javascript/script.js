a6277bc2036ddd5dd259b393488a44bdb08f2658
Nabil OUKDIM <nabil.oukdim@le-campus-numerique.fr>
2020-04-08 14:51:52 +0200

    derniere optimisation du code 25 lignes... :( Bien joué Matthieu :)

--- javascript/script.js
+++ javascript/script.js
@@ -2,20 +2,18 @@ let listButtom = document.querySelector('#declencheur');
 listButtom.addEventListener('click', function () {
     fetch("https://jsonplaceholder.typicode.com/users/1/posts")
         .then(response => response.json())
-        .then(response => dataInFeed(response))
+        .then(response => dataInFeed(response));
     function dataInFeed(response) {
         let title;
         let titleSpan;
         let ParentTitleSpan;
         response.forEach(function (element) {
             title = element.title;
-            console.log(title);
             titleSpan = document.createElement('h1');
             titleSpan.innerHTML = title;
             ParentTitleSpan = document.querySelector('#carousel');
             ParentTitleSpan.appendChild(titleSpan);
         });
-
         $(document).ready(function () {
             $('#carousel').carouFredSel({
                 responsive: true,
