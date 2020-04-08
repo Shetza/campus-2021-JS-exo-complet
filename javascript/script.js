963790d4697ecbfd91cc1765a92c6dba512c5471
Nabil OUKDIM <nabil.oukdim@le-campus-numerique.fr>
2020-04-08 14:28:57 +0200

    optimisation du code

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,3 @@
-function fillTheCarousel(jsondata) {
-}
-
-$(document).ready(function () {
-    // Cliquer sur le bouton "Charger la liste" doit :
-    //  1. Gérer le click sur le bouton
-    //  2. Appeler une api (https://jsonplaceholder.typicode.com/users/1/posts)
-    //  3. Pour chaque élément de la liste, ajouter un élément au futur carousel
-
-    //  4. Déclencher le carousel
-    $('#carousel').carouFredSel({
-        responsive: true,
-        align: "center",
-        direction: 'left'
-    });
-});
-
 let listButtom = document.querySelector('#declencheur');
 listButtom.addEventListener('click', function () {
     fetch("https://jsonplaceholder.typicode.com/users/1/posts")
@@ -32,10 +15,13 @@ listButtom.addEventListener('click', function () {
             ParentTitleSpan = document.querySelector('#carousel');
             ParentTitleSpan.appendChild(titleSpan);
         });
-        $('#carousel').carouFredSel({
-            responsive: true,
-            align: "center",
-            direction: 'left',
+
+        $(document).ready(function () {
+            $('#carousel').carouFredSel({
+                responsive: true,
+                align: "center",
+                direction: 'left'
+            });
         });
     }
 });
